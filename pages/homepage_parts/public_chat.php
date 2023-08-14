f1a49bc8253abdc47959d19bfb79ec497e5f9ab4
AliZibaie <alizibaie1380@gmail.com>
2023-08-15 00:13:37 +0330

    add showMessageJSON()

--- pages/homepage_parts/public_chat.php
+++ pages/homepage_parts/public_chat.php
@@ -2,19 +2,21 @@
 require_once '../app/function.php';
 require_once '../app/helper.php';
 global $online_user;
-$messagesInPublic = json_decode(file_get_contents('../data/public_chat.json'),true);
-$usersInfo = json_decode(file_get_contents('../data/usersInfo.json'),true);
+function showMessageJSON() : void
+{
+    $messagesInPublic = json_decode(file_get_contents('../data/public_chat.json'),true);
+    $usersInfo = json_decode(file_get_contents('../data/usersInfo.json'),true);
 
-$names = [];
-foreach ($messagesInPublic as $key => $messages){
-    $message = $messages['message'];
-    $user = $messages['username'];
-    $name = $message;
-    $names[] = $name;
-    $includes_image = $messages['includes_image'];
-    if ($online_user == $user){
-        if (!$includes_image) {
-            echo "<div class='chat chat-end '>
+    $names = [];
+    foreach ($messagesInPublic as $key => $messages){
+        $message = $messages['message'];
+        $user = $messages['username'];
+        $name = $message;
+        $names[] = $name;
+        $includes_image = $messages['includes_image'];
+        if ($online_user == $user){
+            if (!$includes_image) {
+                echo "<div class='chat chat-end '>
             <div class='chat-image avatar  text-green-700 text-lg'>
             $online_user
             </div>
@@ -22,9 +24,9 @@ foreach ($messagesInPublic as $key => $messages){
             $admin
             </div>
           </div>";
-        }else{
-            $image_name = $messages['image_name'];
-            echo"
+            }else{
+                $image_name = $messages['image_name'];
+                echo"
             <div class='chat chat-end my-10 '>
             <div class='chat-image avatar  text-green-700 text-lg'>
             $user
@@ -40,10 +42,10 @@ foreach ($messagesInPublic as $key => $messages){
             
           </div>
             ";
-        }
-    }else{
-        if (!$includes_image) {
-            echo"
+            }
+        }else{
+            if (!$includes_image) {
+                echo"
             <div class='chat chat-start'>
             <div class='chat-image avatar text-black text-lg'>
             $user
@@ -55,8 +57,8 @@ foreach ($messagesInPublic as $key => $messages){
           </div>
             ";
 
-        }else{
-            $image_name = $messages['image_name'];
+            }else{
+                $image_name = $messages['image_name'];
                 echo"
                 <div class='chat chat-start my-10'>
                 <div class='chat-header text-black text-lg'>
@@ -73,25 +75,9 @@ foreach ($messagesInPublic as $key => $messages){
                 
               </div>
                 ";
+            }
         }
-        }
+    }
 }
 
-//  echo "<pre>";
-//  var_dump($names);
-//  echo "</pre>";
-//  echo "<pre>";
-//  var_dump($_POST);
-//  echo "</pre>";
 
-// foreach ($names as $name) {
-//     if (isset($_PST[$name])) {
-//         echo $name;
-//     }
-// }
-
-// echo '<meta http-equiv="refresh" content="0">';
-
-// deleteMessage("helow");
-// blockByAdmin("hi8");
-// unblockByAdmin("بلبل");
