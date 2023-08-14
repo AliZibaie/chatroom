03a94f05f692d34480817e6611b2d946517108b5
AliZibaie <alizibaie1380@gmail.com>
2023-08-15 01:48:03 +0330

    add MYSQL value to AUTHENTICAION and SHOW_MESSAGE CONSTANT

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
 <?php
-define("SHOW_MESSAGE","JSON");
-define("AUTHENTICATION","JSON");
+//define("SHOW_MESSAGE","JSON");
+//define("AUTHENTICATION","JSON");
+define("AUTHENTICATION","MYSQL");
+define("SHOW_MESSAGE","MYSQL");
